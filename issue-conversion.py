96fa2996dcd8bb6d63ddc2a7d9b8ec783874721b
Gabe Murphy <gabe7murphy@gmail.com>
2020-09-20 23:34:46 -0400

    nested object

--- issue-conversion.py
+++ issue-conversion.py
@@ -2,88 +2,98 @@ import sys
 import os
 import json
 
+def extract_name_and_code(line): 
 
-def extract_name_and_code(line):
+    name_code_pair = {
+        "name": "NONE",
+        "code": ""
+}
 
     code_idx = line.index("[")
 
     name = line[0:code_idx].strip()
     code = line[code_idx:].strip()
 
-    return name, code
+    name_code_pair["name"] = name
+    name_code_pair["code"] = code
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
+    return name_code_pair
 
-if len(sys.argv) < 2:
-    print("Error: Provide Input Text File")
-    sys.exit()
+def main():
 
-print ('Input File:' , str(sys.argv[1]))
+    print ('Number of arguments:', len(sys.argv), 'arguments.')
+    print ('Argument List:', str(sys.argv))
 
-inputactorfilename = sys.argv[1]
+    if len(sys.argv) < 2:
+        print("Error: Provide Input Text File")
+        sys.exit()
 
-with open(inputactorfilename) as f:
-     lines = f.readlines()
+    print ('Input File:' , str(sys.argv[1]))
 
-print (str(len(lines)))
+    inputactorfilename = sys.argv[1]
 
-#strip the end of testactor.txt
-outputactorfilename = inputactorfilename.rsplit('.',1)[0] + '.json'
+    with open(inputactorfilename) as f:
+        lines = f.readlines()
 
-#with open(outputactorfilename, 'w') as f:  #write file and use meld to compare two files
-#   f.writelines(lines) #check  
+    print (str(len(lines)))
 
-issuelist = []
+    #strip the end of testactor.txt
+    outputactorfilename = inputactorfilename.rsplit('.',1)[0] + '.json'
 
+    #with open(outputactorfilename, 'w') as f:  #write file and use meld to compare two files
+    #   f.writelines(lines) #check  
 
-issuedict = {
-    "category": [],
-    "names": [],
-    "code": [],
-    "exculsion phrases": []
-}    
+    issuelist = []
 
+    issuedict = {
+        "category": [],
+        "code": [],
+        "names": "NONE"
+    }  
 
-for line in lines:
+    for line in lines:
 
-    line = line.strip()  #remove leading & trailing spaces
+        line = line.strip()  #remove leading & trailing spaces
 
-    if "</ISSUE>" in line:
-        line = ""
+        if len(line) < 1 or line.startswith("#"):
+            print("Skipping blank or comment")
+            pass  
 
-    if len(line) < 1 or line.startswith("#"):
-        print("Skipping blank or comment")
-        pass  
+        else:   
+             
+            if "</ISSUE>" in line:
+                line = ""
 
-    else:   
+            if "<" in line:
+                opencarrot = line.index("<")
+                closecarrot = line.index(">")
+                category = line[1-opencarrot:closecarrot-1].strip() 
 
-        if "<" in line:
-            opencarrot = line.index("<")
-            closecarrot = line.index(">")
-            category = line[1-opencarrot:closecarrot-1].strip() 
+                issuedict["category"] = category
+    
+            if "~" in line:
+                code_idx = line.index("~")
+                exclusion = line[code_idx:].strip() 
 
-            issuedict["category"] = category
-        
-        if "~" in line:
-            code_idx = line.index("~")
-            exclusion = line[code_idx:].strip() 
+                issuedict["exclusion phrases"] = exclusion
+     
+            if "[" in line:   
+                issue = extract_name_and_code(line)
+                issuedict ["name"].append(agent)
 
-            issuedict["exclusion phrases"] = exclusion
+            else: 
 
-        if "[" in line:   
-                openbracket = line.index("[")
-                closebracket = line.index("]")
-                code = line[openbracket+1:closebracket].strip() 
+                if issuedict["names"] != "NONE":
+                    issuelist.append(issuedict)
+                    issuedict = issuedict = {
+                    "category": [],
+                    "code": [],
+                    "names": issue,
+                     "exclusion phrases": []
 
-                issuedict["code"] = code
-                                      
+              }  
 
-
-issuelist.append(issuedict)    
-
-with open(outputactorfilename, 'w') as f:
-    json.dump(issuelist, f, ensure_ascii=False, indent=4)
-
+    with open(outputactorfilename, 'w') as f:
+        json.dump(issuelist, f, ensure_ascii=False, indent=4)
 
     
